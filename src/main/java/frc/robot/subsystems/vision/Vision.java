a540d4e44e9417a889cee711106e3267fbd880a4
Saketh <126517689+SAKETH11111@users.noreply.github.com>
2024-01-18 16:23:25 -0600

    Added code to load resource for the AprilTagFieldLayout

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -2,15 +2,16 @@ package frc.robot.subsystems.vision;
 
 import java.util.Optional;
 
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
@@ -23,20 +24,31 @@ public class Vision extends SubsystemBase {
 
     public Vision(VisionIO io) {
         this.io = io;
+
+        AprilTagFieldLayout aprilTagFieldLayout = null;
+
+        try {
+            // Load the AprilTag field layout from the resource file
+            aprilTagFieldLayout =
+                AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+            System.out.println("Loaded the AprilTag field layout from the resource file");
+        } catch (Exception e) {
+            System.out.println("Exception encountered: " + e.getMessage());
+        }
+
         camera = new PhotonCamera("frontCam");
         poseEstimator = new PhotonPoseEstimator(
-                /* AprilTagFieldLayout */ null,
+                aprilTagFieldLayout,
                 PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
                 camera,
-                /* Transform3d */ new Transform3d(Units.inchesToMeters(12), 0.0, Units.inchesToMeters(5.25), new Rotation3d()));
+                new Transform3d(Units.inchesToMeters(12), 0.0, Units.inchesToMeters(5.25), new Rotation3d()));
         poseEstimator.setMultiTagFallbackStrategy(PoseStrategy.CLOSEST_TO_LAST_POSE);
     }
 
     @Override
     public void periodic() {
-        io.updateInputs(inputs); 
+        io.updateInputs(inputs);
         Optional<EstimatedRobotPose> estimatedPose = getEstimatedGlobalPose(memory);
-        
 
         if (estimatedPose.isPresent()) {
             memory = estimatedPose.get().estimatedPose.toPose2d();
