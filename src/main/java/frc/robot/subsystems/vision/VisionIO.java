3454d93d989ee4599663e392052124a2a2b5751b
Saketh <126517689+SAKETH11111@users.noreply.github.com>
2024-01-18 16:28:14 -0600

    Update VisionIO.java

--- src/main/java/frc/robot/subsystems/vision/VisionIO.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIO.java
@@ -14,17 +14,8 @@ public interface VisionIO {
     public double captureTimestamp = 0.0;
     public double[] cornerX = new double[] {};
     public double[] cornerY = new double[] {};
-    public boolean simpleValid = false;
-    public double simpleAngle = 0.0;
-    
   }
 
   /** Updates the set of loggable inputs. */
   public default void updateInputs(VisionIOInputs inputs) {}
-
-  /** Enabled or disabled vision LEDs. */
-  public default void setLeds(boolean enabled) {}
-
-  /** Sets the pipeline number. */
-  public default void setPipeline(int pipeline) {}
 }
