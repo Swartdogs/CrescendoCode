9d644af0d4716063d102c5aaa401e8c0fc8eb057
eden <edenreicherts@gmail.com>
2024-02-08 14:11:44 -0600

    Added constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,6 +72,8 @@ public final class Constants
 
     public static class Climb
     {
+        public static final double MIN_EXTENSION                 = 0.0;
+        public static final double MAX_EXTENSION                 = 24.0;
         public static final double CLIMB_SENSOR_RATE_DEG_PER_SEC = 360;
         public static final double CLIMB_SENSOR_DEG_PER_INCH     = 60;
         public static final double LEFT_ZERO_OFFSET              = 0.0;
