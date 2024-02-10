b1af0a2e376385af68143b7c98f3aa8f62f6f7b0
Cedric Liu <122331302+L-Cedric@users.noreply.github.com>
2024-02-09 20:01:08 -0600

    Commands starting and stopping intake and notepath (#14)
    Co-authored-by: Collin McIntyre <crm11396@gmail.com>

--- src/main/java/frc/robot/subsystems/notepath/NotepathIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/notepath/NotepathIOSparkMax.java
@@ -1,21 +1,26 @@
 package frc.robot.subsystems.notepath;
 
 import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import frc.robot.Constants;
 
 public class NotepathIOSparkMax implements NotepathIO
 {
-    private CANSparkMax _notepathSparkMax;
-    private CANSparkMax _followerSparkMax;
+    private CANSparkMax  _notepathSparkMax;
+    private CANSparkMax  _followerSparkMax;
+    private DigitalInput _noteSensor;
 
-    @SuppressWarnings("resource")
     public NotepathIOSparkMax()
     {
         _notepathSparkMax = new CANSparkMax(Constants.CAN.NOTEPATH_LEADER, MotorType.kBrushless);
         _followerSparkMax = new CANSparkMax(Constants.CAN.NOTEPATH_FOLLOWER, MotorType.kBrushless);
         _followerSparkMax.follow(_notepathSparkMax, true);
+
+        _noteSensor = new DigitalInput(Constants.DIO.NOTE_SENSOR);
     }
 
     @Override
@@ -26,6 +31,8 @@ public class NotepathIOSparkMax implements NotepathIO
 
         inputs.followerNotepathAppliedVolts = _followerSparkMax.getAppliedOutput() * _followerSparkMax.getBusVoltage();
         inputs.followerNotepathCurrentAmps  = new double[] { _followerSparkMax.getOutputCurrent() };
+
+        inputs.sensorTripped = !_noteSensor.get();
     }
 
     @Override
