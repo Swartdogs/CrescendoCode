e9b31b0adb024bf36dc5207589b43ca1801d1311
eden <edenreicherts@gmail.com>
2024-03-02 15:41:13 -0600

    WIP

--- src/main/java/frc/robot/util/DeferredInstantCommand.java
+++ src/main/java/frc/robot/util/DeferredInstantCommand.java
@@ -1,36 +1,30 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-package frc.robot.commands;
+package frc.robot.util;
+
+import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class DeferCommand extends Command
+public class DeferredInstantCommand extends Command
 {
-    public DeferCommand()
-    {
+    private final Supplier<Command> _supplier;
 
+    public DeferredInstantCommand(Supplier<Command> supplier)
+    {
+        _supplier = supplier;
     }
 
     @Override
     public void initialize()
     {
+        _supplier.get().schedule();
     }
 
-    @Override
-    public void execute()
-    {
-    }
-
-    @Override
-    public void end(boolean interrupted)
-    {
-    }
-
-    // Returns true when the command should end.
     @Override
     public boolean isFinished()
     {
-        return false;
+        return true;
     }
 }
