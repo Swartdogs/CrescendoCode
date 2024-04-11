b4f3f227c98c7912fbc33cd31f12e7e80ec967d9
Cedric Liu <25cedliu@student.cfschools.org>
2024-04-10 22:49:03 -0500

    Experimental LED Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,8 +5,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.CompositeCommands;
 import frc.robot.subsystems.leds.LED;
 
-import static frc.robot.Constants.LED.*;
-
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
